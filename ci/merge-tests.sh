6e9d6d8f45ff251c760a75e7b55e22483a762243
msc22h2 <s290705@studenti.polito.it>
2023-03-22 14:27:39 +0100

    Unused code removed and code commented

--- ci/merge-tests.sh
+++ ci/merge-tests.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-rm -rf run.log
-touch run.log
-i=0
-for FILE in $(ls log) #$(ls log | find ../CHERI-Litmus/tests/riscv-tests/non-mixed-size/ | grep Coi-Rfi)
+#path of the the file where tests will be merged
+dest=run.log
+
+rm -rf $dest
+touch $dest
+
+for FILE in $(ls log) 
 do 
-	#FILE=$(basename $FILE .litmus).log
+
+	#check that the litmus test terminated correctly
 	if grep -q Time log/$FILE; then 
-		cat log/$FILE >> run.log
-		echo "" >> run.log
+		cat log/$FILE >> $dest
+		echo "" >> $dest
 		i=$((i+1))
 	else
 		echo "'Time' not found in $FILE"
 	fi
+
 done 
 echo "Num of tests = $i"
--- ci/riscv-litmus-tests.sh
+++ ci/riscv-litmus-tests.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-#rm log2/*
-#cp log/* log2/
+#before executing this script it is assumed that limtus tests
+#are already compiled end the corresponding elf files are 
+#in the $riscv-litmus-test-dir specified in the main Makefile
+
+
+#remove all output logs
 rm log/*
-# cd ../CHERI-Litmus/binaries
-# ./clean.sh
-# ./make-riscv.sh ../tests/riscv-tests/non-mixed-size/
-# cd ../../cva6_forked
+
 cva6 make clean
+#save all tests names in a file
 cva6 make find-litmus
+#run all litmus tests in parallel (with 8 threads)
 cva6 make -j8 run-litmus-tests batch-mode=1
+
+#merge all logs in a unique text file
 ./ci/merge-tests.sh 
